922201211921659ff9f3bb7c4f1e2aeb90350534
Dima Savchenko <savvaq@gmail.com>
2021-03-21 14:01:18 -0400

    added controller methods and one view which still doesn't work

--- app/controllers/pups_controller.rb
+++ app/controllers/pups_controller.rb
@@ -1,2 +1,34 @@
 class PupsController < ApplicationController
+    def new
+        @pup = Pup.new
+    end
+
+    def create
+        @pup = Pup.new(pup_params)
+        @pup.save
+    end
+
+    def edit
+        @pup = Pup.find(params[:id])
+    end
+
+    def update
+        @pup = Pup.find(params[:id])
+        @pup.update(pup_params)
+    
+        redirect_to pup_path(@pup)
+    end
+
+    def destroy
+        @pup = Pup.find(params[:id])
+        @pup.destroy
+    
+        redirect_to pups_path
+      end
+
+    private
+
+    def pup_params
+        params.require(:pup).permit(:name, :coat, :gender, :weight)
+      end
 end
