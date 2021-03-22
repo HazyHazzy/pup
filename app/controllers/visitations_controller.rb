bfbe6af299d918972276a0e30ad4e138495df28f
xinglin2016 <lixinglin0829@gmail.com>
2021-03-22 16:54:45 -0400

    add views under visitation and page

--- app/controllers/visitations_controller.rb
+++ app/controllers/visitations_controller.rb
@@ -1,2 +1,62 @@
 class VisitationsController < ApplicationController
+  before_action :set_visitation, only: [:edit, :update]
+
+  def index
+  end
+
+  def new
+    @breeder = Breeder.find(params[:breeder_id])
+    @visitation = Visitation.new
+    # raise
+  end
+
+  def create
+    @breeder = Breeder.find(params[:breeder_id])
+    @visitation = Visitation.new(rental_params)
+    @visitation.user = current_user
+    @visitation.breeder = @breeder
+    @visitation.status = "pending"
+    @visitation.save
+    # if @visitation.save
+    #   redirect_to rentals_path
+    # else
+    #   render :new
+    # end
+    # raise
+  end
+
+  def edit; end
+
+  def update
+    @visitation.update(visit_params)
+    redirect_to visitation_path(@visitation)
+  end
+
+  # def approve
+  #   @breeder = Breeder.find(params[:breeder_id])
+  #   @visitation = Visitation.find_by_id(params[:id])
+  #   if @visitation.user_id == current_user.id
+  #     @visitation.status = "approved"
+  #     @visitation.save
+  #   end
+  # end
+
+  # def decline
+  #   @breeder = Breeder.find(params[:breeder_id])
+  #   @visitation = Visitation.find_by_id(params[:id])
+  #   if @visitation.user_id == current_user.id
+  #     @visitation.status = "decline"
+  #     @visitation.save
+  #   end
+  # end
+
+  private
+
+  def set_visitation
+    @visitation = Visitation.find(params[:id])
+  end
+
+  def visit_params
+    params.require(:visitation).permit(:status)
+  end
 end
