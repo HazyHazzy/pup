73377b209303b589feac248345c70b8a71bfe2a8
xinglin2016 <lixinglin0829@gmail.com>
2021-04-01 17:20:31 -0400

    add user name in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.new(
   email: Faker::Internet.email,
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
   password: "123456"
 )
 puts "#{user} created"
