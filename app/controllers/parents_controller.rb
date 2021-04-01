a23364dfe57ac56166f422bdc19c3fc171575cb2
Dmytro Savchenko <savvaq@gmail.com>
2021-03-31 20:09:03 -0400

    Finished new and create methods in controller and created view to create new parent

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,12 +1,21 @@
 class ParentsController < ApplicationController
   
   def new
+    @breeder = Breeder.find(params[:breeder_id])
+    @breed = Breed.all
     @parent = Parent.new
   end
 
   def create
-    @parent = Parent.new
-    @parent.save
+    @breeder = Breeder.find(params[:breeder_id])
+    @parent = Parent.new(parent_params)
+    @parent.breeder = @breeder
+    
+    if @parent.save
+      redirect_to breeder_parent_path(@breeder, @parent)
+    else
+      render :new
+    end
   end
 
   def show
@@ -30,7 +39,7 @@ class ParentsController < ApplicationController
   private
 
   def parent_params
-    params.require(:parent).permit(:name, :weight, :coat, :description, :gender)
+    params.require(:parent).permit(:name, :weight, :coat, :description, :gender, :breed_id)
   end
 
 end
