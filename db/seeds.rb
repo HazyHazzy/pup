a5364dfab4c742c22b3b0a6229c8c2bec69a1f50
xinglin2016 <lixinglin0829@gmail.com>
2021-04-10 15:33:53 -0400

    modify the seed file to make each litter have parents and puppies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,8 @@ puts "Creating user"
     last_name: Faker::Name.last_name,
     password: "123456"
     )
-  puts "#{user} created"
-  user.save
+  puts "#{user.first_name} created"
+  user.save!
 end
 
 def dog
@@ -67,7 +67,7 @@ end
   puts "Created breed #{breed.name}"
 end
 
-3.times do
+1.times do
   breeder = Breeder.new(
     name: Faker::Name.name,
     city: Faker::Address.city,
@@ -90,14 +90,14 @@ end
       that come up. I am trying to keep meticulous records of my puppies height, weight, and the
       development in their bodies.', 'I will greet you in my house and not somewhere else.
       My puppies live in the house and be handled often, as pet who is born into family life has a
-      better shot at growing up relaxed and friendly.' 'Hello, The parents are be on site, and you will be able
+      better shot at growing up relaxed and friendly.', 'Hello, The parents are be on site, and you will be able
       to meet them, meeting the father may not be possible, but you should certainly meet the mother.',
       'My place is clean and safe, and supplied with fresh water, beds, and toys. There is a toilet area
       in the puppy living quarters for housetraining. There will be minimal numbers of litters from mom,
       and the number of litters available for adoption will be limited.', 'I will show knowledge of the
       breed, and be honest about its advantages and drawbacks, whether that means a tendency to develop
       certain health problems or a temperament. I will take the dog back if you are unable to care for it.'].sample,
-    user_id: User.find(rand(1..User.ids.length)).id
+    user_id: User.ids[0]
     )
 
   file = File.open("app/assets/images/breeder_pic.jpg")
@@ -105,14 +105,28 @@ end
   breeder.save!
   puts "Created breeder #{breeder.name}"
 
-  2.times do
+  1.times do
     parent = Parent.new(
       name: Faker::Creature::Dog.name,
       coat: Faker::Creature::Dog.coat_length,
       gender: Faker::Creature::Dog.gender,
       weight: rand(10..40),
       description: dog_description,
-      breeder_id: Breeder.find(rand(1..Breeder.ids.length)).id,
+      breeder_id: Breeder.ids[0],
+      breed_id: Breed.find(rand(1..Breed.ids.length)).id
+      )
+    file = URI.open(dog[0]["url"])
+    parent.photo.attach(io: file, filename: 'dalmatian_adult.jpg', content_type: 'image/jpg')
+    parent.save!
+    puts "Created parent #{parent.name}"
+
+    parent = Parent.new(
+      name: Faker::Creature::Dog.name,
+      coat: Faker::Creature::Dog.coat_length,
+      gender: Faker::Creature::Dog.gender,
+      weight: rand(10..40),
+      description: dog_description,
+      breeder_id: Breeder.ids[0],
       breed_id: Breed.find(rand(1..Breed.ids.length)).id
       )
     file = URI.open(dog[0]["url"])
@@ -124,22 +138,218 @@ end
       litter = Litter.new(
         name: Faker::Creature::Dog.name,
         birth_date: Faker::Date.birthday(min_age: 0, max_age: 13),
-        stud_id: Parent.find(rand(1..Parent.ids.length)).id,
-        mom_id: Parent.find(rand(1..Parent.ids.length)).id,
-        breeder_id: Breeder.find(rand(1..Breeder.ids.length)).id
+        stud_id: Parent.ids[0],
+        mom_id: Parent.ids[0],
+        breeder_id: Breeder.ids[0]
         )
       file = File.open("app/assets/images/litter_pic.jpg")
       litter.photo.attach(io: file, filename: 'litter_pic.jpg', content_type: 'image/jpg')
       litter.save!
       puts "Created litter #{litter.name}"
 
-      2.times do
+      1.times do
         puppy = Puppy.new(
           name: Faker::Creature::Dog.name,
           coat: Faker::Creature::Dog.coat_length,
           gender: Faker::Creature::Dog.gender,
           weight: rand(10..40),
-          litter_id: Litter.find(rand(1..Litter.ids.length)).id,
+          litter_id: Litter.ids[0],
+          breed_id: Breed.find(rand(1..Breed.ids.length)).id
+          )
+        file = URI.open(dog[0]["url"])
+        puppy.photo.attach(io: file, filename: 'Breed_bulldog.jpg', content_type: 'image/jpg')
+        puppy.save!
+        puts "Created puppy #{puppy.name}"
+      end
+    end
+  end
+end
+
+1.times do
+  breeder = Breeder.new(
+    name: Faker::Name.name,
+    city: Faker::Address.city,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    website_url: Faker::Internet.url,
+    about_us: ['I am informative and honest about a breed’s physical and
+      temperamental traits. And I can speak knowledgeably about genetic diseases
+      that might affect my breed.', 'I am friendly and willing
+      to establish a good rapport with you. I am an excellent
+      resource and breed mentor for you throughout the life of your puppy, and
+      encourage you to call if your dog has a crisis at any stage of its life.',
+      'I am willing to answer any questions you have, and I will
+      ask many of you as well. I want to make sure my puppies are going
+      to good homes, with people who know what to expect and have made all the necessary
+      preparations.', 'I confirm that my puppies are getting the proper
+      nutrition and are protected from the elements. Their whelping box is designed to protect
+      them in their early life from drafts or from being accidentally crushed by their mother.',
+      'I have a lot of expenses through veterinary checks, shots, and any other health care expenses
+      that come up. I am trying to keep meticulous records of my puppies height, weight, and the
+      development in their bodies.', 'I will greet you in my house and not somewhere else.
+      My puppies live in the house and be handled often, as pet who is born into family life has a
+      better shot at growing up relaxed and friendly.', 'Hello, The parents are be on site, and you will be able
+      to meet them, meeting the father may not be possible, but you should certainly meet the mother.',
+      'My place is clean and safe, and supplied with fresh water, beds, and toys. There is a toilet area
+      in the puppy living quarters for housetraining. There will be minimal numbers of litters from mom,
+      and the number of litters available for adoption will be limited.', 'I will show knowledge of the
+      breed, and be honest about its advantages and drawbacks, whether that means a tendency to develop
+      certain health problems or a temperament. I will take the dog back if you are unable to care for it.'].sample,
+    user_id: User.ids[1]
+    )
+
+  file = File.open("app/assets/images/breeder_pic.jpg")
+  breeder.photo.attach(io: file, filename: 'breeder_pic.jpg', content_type: 'image/jpg')
+  breeder.save!
+  puts "Created breeder #{breeder.name}"
+
+  1.times do
+    parent = Parent.new(
+      name: Faker::Creature::Dog.name,
+      coat: Faker::Creature::Dog.coat_length,
+      gender: Faker::Creature::Dog.gender,
+      weight: rand(10..40),
+      description: dog_description,
+      breeder_id: Breeder.ids[1],
+      breed_id: Breed.find(rand(1..Breed.ids.length)).id
+      )
+    file = URI.open(dog[0]["url"])
+    parent.photo.attach(io: file, filename: 'dalmatian_adult.jpg', content_type: 'image/jpg')
+    parent.save!
+    puts "Created parent #{parent.name}"
+
+    parent = Parent.new(
+      name: Faker::Creature::Dog.name,
+      coat: Faker::Creature::Dog.coat_length,
+      gender: Faker::Creature::Dog.gender,
+      weight: rand(10..40),
+      description: dog_description,
+      breeder_id: Breeder.ids[1],
+      breed_id: Breed.find(rand(1..Breed.ids.length)).id
+      )
+    file = URI.open(dog[0]["url"])
+    parent.photo.attach(io: file, filename: 'dalmatian_adult.jpg', content_type: 'image/jpg')
+    parent.save!
+    puts "Created parent #{parent.name}"
+
+    1.times do
+      litter = Litter.new(
+        name: Faker::Creature::Dog.name,
+        birth_date: Faker::Date.birthday(min_age: 0, max_age: 13),
+        stud_id: Parent.ids[1],
+        mom_id: Parent.ids[1],
+        breeder_id: Breeder.ids[1]
+        )
+      file = File.open("app/assets/images/litter_pic.jpg")
+      litter.photo.attach(io: file, filename: 'litter_pic.jpg', content_type: 'image/jpg')
+      litter.save!
+      puts "Created litter #{litter.name}"
+
+      1.times do
+        puppy = Puppy.new(
+          name: Faker::Creature::Dog.name,
+          coat: Faker::Creature::Dog.coat_length,
+          gender: Faker::Creature::Dog.gender,
+          weight: rand(10..40),
+          litter_id: Litter.ids[1],
+          breed_id: Breed.find(rand(1..Breed.ids.length)).id
+          )
+        file = URI.open(dog[0]["url"])
+        puppy.photo.attach(io: file, filename: 'Breed_bulldog.jpg', content_type: 'image/jpg')
+        puppy.save!
+        puts "Created puppy #{puppy.name}"
+      end
+    end
+  end
+end
+
+1.times do
+  breeder = Breeder.new(
+    name: Faker::Name.name,
+    city: Faker::Address.city,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    website_url: Faker::Internet.url,
+    about_us: ['I am informative and honest about a breed’s physical and
+      temperamental traits. And I can speak knowledgeably about genetic diseases
+      that might affect my breed.', 'I am friendly and willing
+      to establish a good rapport with you. I am an excellent
+      resource and breed mentor for you throughout the life of your puppy, and
+      encourage you to call if your dog has a crisis at any stage of its life.',
+      'I am willing to answer any questions you have, and I will
+      ask many of you as well. I want to make sure my puppies are going
+      to good homes, with people who know what to expect and have made all the necessary
+      preparations.', 'I confirm that my puppies are getting the proper
+      nutrition and are protected from the elements. Their whelping box is designed to protect
+      them in their early life from drafts or from being accidentally crushed by their mother.',
+      'I have a lot of expenses through veterinary checks, shots, and any other health care expenses
+      that come up. I am trying to keep meticulous records of my puppies height, weight, and the
+      development in their bodies.', 'I will greet you in my house and not somewhere else.
+      My puppies live in the house and be handled often, as pet who is born into family life has a
+      better shot at growing up relaxed and friendly.', 'Hello, The parents are be on site, and you will be able
+      to meet them, meeting the father may not be possible, but you should certainly meet the mother.',
+      'My place is clean and safe, and supplied with fresh water, beds, and toys. There is a toilet area
+      in the puppy living quarters for housetraining. There will be minimal numbers of litters from mom,
+      and the number of litters available for adoption will be limited.', 'I will show knowledge of the
+      breed, and be honest about its advantages and drawbacks, whether that means a tendency to develop
+      certain health problems or a temperament. I will take the dog back if you are unable to care for it.'].sample,
+    user_id: User.ids[2]
+    )
+
+  file = File.open("app/assets/images/breeder_pic.jpg")
+  breeder.photo.attach(io: file, filename: 'breeder_pic.jpg', content_type: 'image/jpg')
+  breeder.save!
+  puts "Created breeder #{breeder.name}"
+
+  1.times do
+    parent = Parent.new(
+      name: Faker::Creature::Dog.name,
+      coat: Faker::Creature::Dog.coat_length,
+      gender: Faker::Creature::Dog.gender,
+      weight: rand(10..40),
+      description: dog_description,
+      breeder_id: Breeder.ids[2],
+      breed_id: Breed.find(rand(1..Breed.ids.length)).id
+      )
+    file = URI.open(dog[0]["url"])
+    parent.photo.attach(io: file, filename: 'dalmatian_adult.jpg', content_type: 'image/jpg')
+    parent.save!
+    puts "Created parent #{parent.name}"
+
+    parent = Parent.new(
+      name: Faker::Creature::Dog.name,
+      coat: Faker::Creature::Dog.coat_length,
+      gender: Faker::Creature::Dog.gender,
+      weight: rand(10..40),
+      description: dog_description,
+      breeder_id: Breeder.ids[2],
+      breed_id: Breed.find(rand(1..Breed.ids.length)).id
+      )
+    file = URI.open(dog[0]["url"])
+    parent.photo.attach(io: file, filename: 'dalmatian_adult.jpg', content_type: 'image/jpg')
+    parent.save!
+    puts "Created parent #{parent.name}"
+
+    1.times do
+      litter = Litter.new(
+        name: Faker::Creature::Dog.name,
+        birth_date: Faker::Date.birthday(min_age: 0, max_age: 13),
+        stud_id: Parent.ids[2],
+        mom_id: Parent.ids[2],
+        breeder_id: Breeder.ids[2]
+        )
+      file = File.open("app/assets/images/litter_pic.jpg")
+      litter.photo.attach(io: file, filename: 'litter_pic.jpg', content_type: 'image/jpg')
+      litter.save!
+      puts "Created litter #{litter.name}"
+
+      1.times do
+        puppy = Puppy.new(
+          name: Faker::Creature::Dog.name,
+          coat: Faker::Creature::Dog.coat_length,
+          gender: Faker::Creature::Dog.gender,
+          weight: rand(10..40),
+          litter_id: Litter.ids[2],
           breed_id: Breed.find(rand(1..Breed.ids.length)).id
           )
         file = URI.open(dog[0]["url"])
